0d9ae962b5c39994beb4f1b9bfdf51997b794269
Lukas Buenger <lukasbuenger@gmail.com>
2018-08-28 13:05:37 +0200

    navi

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,46 +1,51 @@
 import React from 'react'
 import { css } from 'glamor'
-import { compose } from 'react-apollo'
+import { compose } from 'react-apollo'
+import { withRouter } from 'next/router'
+
 import {
   createContainer,
-  createTile
+  createTile,
 } from './Grid'
 import Me from '../Auth/Me'
 import {
   BrandMark,
   Interaction,
-  colors
+  colors,
 } from '@project-r/styleguide'
 import routes from '../../server/routes'
 const { Link } = routes
 
 const link = css({
   textDecoration: 'none',
-  color: colors.primary,
+  color: '#000',
   ':visited': {
-    color: colors.primary
+    color: '#000',
   },
   ':hover': {
-    color: colors.secondary
-  }
+    color: colors.secondary,
+  },
+  '[disabled]': {
+    color: colors.primary,
+  },
 })
 
 const Header = compose(
   createTile(
     {
-      flex: '0 0 80px'
+      flex: '0 0 80px',
     },
     {
       style: {
         borderBottom: `1px solid ${
           colors.divider
-        }`
-      }
+        }`,
+      },
     }
   ),
   createContainer({
     direction: 'row',
-    justifyContent: 'stretch'
+    justifyContent: 'stretch',
   })
 )('header')
 
@@ -48,68 +53,84 @@ const HeaderSection = compose(
   createTile(),
   createContainer({
     direction: 'column',
-    justifyContent: 'center'
+    justifyContent: 'center',
   })
 )('div')
 
 const logoStyles = {
   width: '50px',
   marginLeft: '5px',
-  display: 'inline-block'
+  display: 'inline-block',
 }
 
 const navLinkStyles = {
   display: 'inline-block',
   marginRight: '12px',
-  cursor: 'pointer'
+  cursor: 'pointer',
 }
 
-export default ({ ...props }) => (
-  <Header {...props}>
-    <HeaderSection flex="0 0 70px">
-      <span style={logoStyles}>
-        <BrandMark />
-      </span>
-    </HeaderSection>
-    <HeaderSection flex="1 1 auto">
-      <Interaction.H2>Admin</Interaction.H2>
-      <nav>
-        <Link route="users">
-          <a
-            className={`${link}`}
-            style={navLinkStyles}
-          >
-            Users
-          </a>
-        </Link>
-        <Link route="payments">
-          <a
-            className={`${link}`}
-            style={navLinkStyles}
-          >
-            Payments
-          </a>
-        </Link>
-        <Link route="postfinance-payments">
-          <a
-            className={`${link}`}
-            style={navLinkStyles}
-          >
-            Postfinance Payments
-          </a>
-        </Link>
-        <Link route="merge-users">
-          <a
-            className={`${link}`}
-            style={navLinkStyles}
-          >
-            Users zusammenführen
-          </a>
-        </Link>
-      </nav>
-    </HeaderSection>
-    <HeaderSection flex="0 0 200px">
-      <Me />
-    </HeaderSection>
-  </Header>
+export default withRouter(
+  ({ router, ...props }) => {
+    const { pathname } = router
+    return (
+      <Header {...props}>
+        <HeaderSection flex="0 0 70px">
+          <span style={logoStyles}>
+            <BrandMark />
+          </span>
+        </HeaderSection>
+        <HeaderSection flex="1 1 auto">
+          <Interaction.H2>Admin</Interaction.H2>
+          <nav>
+            <Link route="users">
+              <a
+                className={`${link}`}
+                style={navLinkStyles}
+                disabled={pathname === '/users'}
+              >
+                Users
+              </a>
+            </Link>
+            <Link route="payments">
+              <a
+                className={`${link}`}
+                style={navLinkStyles}
+                disabled={
+                  pathname === '/payments'
+                }
+              >
+                Payments
+              </a>
+            </Link>
+            <Link route="postfinance-payments">
+              <a
+                className={`${link}`}
+                style={navLinkStyles}
+                disabled={
+                  pathname ===
+                  '/postfinance-payments'
+                }
+              >
+                Postfinance Payments
+              </a>
+            </Link>
+            <Link route="merge-users">
+              <a
+                className={`${link}`}
+                style={navLinkStyles}
+                disabled={
+                  pathname === '/merge-users'
+                }
+              >
+                Users zusammenführen
+              </a>
+            </Link>
+          </nav>
+        </HeaderSection>
+        <HeaderSection flex="0 0 200px">
+          <Me />
+        </HeaderSection>
+      </Header>
+    )
+  }
 )
